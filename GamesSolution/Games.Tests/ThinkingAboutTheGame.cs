a5a2cbfd4b18fef01391fae447dcc7f80458be8c
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-21 12:33:31 -0400

    Lunch

---
+++ GamesSolution/Games.Tests/ThinkingAboutTheGame.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Games.Tests;
+
+public class ThinkingAboutTheGame
+{
+    [Fact]
+    public void DuplicateNamesAreNotAllowed()
+    {
+        //given
+        var game = new BowlingGame();
+
+        game.addPlayer("Jim", 120);
+
+        //then or when
+        Assert.Throws<PlayerAlreadyAddedToGameException>(() => game.addPlayer("Jim", 200));
+
+    }
+}
---
+++ GamesSolution/Games/BowlingGame.cs
@@ -0,0 +1,29 @@
+﻿namespace Games;
+
+public class BowlingGame
+{
+    private readonly List<Player> _player;
+    public BowlingGame()
+    {
+        _player = new();
+    }
+
+    public void addPlayer(string name, int score)
+    {
+        if (PlayerExsists(name))
+        {
+            throw new PlayerAlreadyAddedToGameException();
+        }
+        else
+        {
+            _player.Add(new Player(name, score));
+        }
+    }
+
+    private bool PlayerExsists(string name)
+    {
+        return _player.Any(p => p.name == name || p.name.ToLower() == name.ToLower() || p.name.ToUpper() == name.ToUpper() || p.name.Trim() == name.Trim());
+    }
+}
+
+public record Player(string name, int score);
