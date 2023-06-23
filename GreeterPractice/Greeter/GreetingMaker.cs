c678143f0d746589446f122418b8abb765aec297
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-23 14:13:14 -0400

    refactor code

--- GreeterPractice/Greeter/GreetingMaker.cs
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -8,7 +8,25 @@
 
         public string Greet(string? name, params string[] names)
         {
-            if (names.Length > 1)
+            var output = "";
+            if (names.Length >= 1)
+            {
+                output = MultipleNames(name, names); 
+            } 
+            else
+            {
+                output = RegularFormat(name);
+            }
+            return output;   
+        }
+        //Format string for multiple names
+        protected string MultipleNames(string? name, string[] names)
+        {
+            if (names.Length == 1) 
+            { 
+                return $"Hello, {name} and {names.First()}!";
+            }
+            else
             {
                 var output = $"Hello, {name}";
                 foreach (string n in names)
@@ -24,9 +42,11 @@
                 }
                 return output;
             }
-            if (names.Length  == 1) { return $"Hello, {name} and {names.First()}!"; }
-
-            if (name == null) { return "Hello, Buddy!";  }
+        }
+        //Format for singular name
+        protected string RegularFormat(string? name) 
+        {
+            if (name == null) { return "Hello, Buddy!"; }
 
             if (name.ToUpper() == name)
             {
